cb6f59ae1f946644de69576dc2c7c379eb3875b1
Wessam Abozeid <wessam@uchicago.edu>
2018-11-12 05:07:42 +0000

    fourth

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :username, presence: true, uniqueness: true
   
-  belongs_to :photo
+  has_many :photos
   
   has_many :likes, :dependent => :destroy
   has_many :liked_photos, :through => :likes, :source => :photo
